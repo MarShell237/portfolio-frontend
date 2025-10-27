88fb9dcfd1af2d74d758ce3ba398f1d8c67a0122
MarShell237 <marceldjiofack@outlook.com>
2025-10-27 21:29:31 +0100

    add dockerfile test to deploy 3

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,13 @@
 <template>
-    <div v-html="data" class="p-4"/>
+    <div v-html="data1" class="p-4"/>
+    <div>{{ data2 }}</div>
 </template>
 
 <script lang="ts" setup>
     const config = useRuntimeConfig()
-    const { data } = await useFetch(`${config.public.apiUrl}`)
+const { data: data1 } = await useFetch(`${config.public.apiUrl}`)
+const { data: data2 } = await useFetch(`${config.public.apiUrl}/api/v1/tags`)
+
     console.log(config.public.apiUrl)
 </script>
 
