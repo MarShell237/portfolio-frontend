2259b2fc2ea4633dcc29698333f77f196bb42e53
MarShell237 <marceldjiofack@outlook.com>
2025-11-16 04:28:19 +0100

    add tooltips on technologies section

--- app/components/ModeToggle.vue
+++ app/components/ModeToggle.vue
@@ -1,30 +1,14 @@
 <script setup lang="ts">
-import { Icon } from '@iconify/vue'
-import { Button } from '@/components/ui/button'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+    import { Icon } from '@iconify/vue'
+    import { Button } from '@/components/ui/button'
 
-const colorMode = useColorMode()
+    const colorMode = useColorMode()
 </script>
 
 <template>
-  <DropdownMenu>
-    <DropdownMenuTrigger as-child>
-      <Button variant="outline">
+    <Button variant="outline" @click="colorMode.value = colorMode.value === 'light' ? 'dark' : 'light'">
         <Icon icon="radix-icons:moon" class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         <Icon icon="radix-icons:sun" class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         <span class="sr-only">Basculer le thème</span>
-      </Button>
-    </DropdownMenuTrigger>
-    <DropdownMenuContent align="end">
-      <DropdownMenuItem @click="colorMode.preference = 'light'">
-        Clair
-      </DropdownMenuItem>
-      <DropdownMenuItem @click="colorMode.preference = 'dark'">
-        Sombre
-      </DropdownMenuItem>
-      <DropdownMenuItem @click="colorMode.preference = 'system'">
-        Système
-      </DropdownMenuItem>
-    </DropdownMenuContent>
-  </DropdownMenu>
+    </Button>
 </template>
--- app/components/sections/TechStackSection.vue
+++ app/components/sections/TechStackSection.vue
@@ -3,29 +3,45 @@
         <h2 class="text-3xl font-bold text-balance">Stack Technologique</h2>
         <p class="lg:text-base text-justify py-6">Technologies que j'utilise pour créer des applications web, mobile et desktop performantes, élégantes et évolutives.</p>
         <div class="flex flex-wrap justify-center gap-8 p-6">
-            <NuxtLink
-                v-for="(techno, index) in technoItems"
-                :key="index"
-                :to="techno.link"
-                target="_blank"
-                rel="noopener noreferrer"
-                class="flex flex-col items-center gap-2 transition-transform duration-200 hover:scale-110"
-            >
-                <img :src="techno.image" :alt="techno.name" class="h-16 w-16"/>
-                <span class="text-sm font-medium">{{ techno.name }}</span>
-            </NuxtLink>
+            <TooltipProvider v-for="(techno, index) in technoItems">
+                <Tooltip>
+                    <TooltipTrigger as-child>
+                        <NuxtLink
+                            :key="index"
+                            :to="techno.link"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            class="flex flex-col items-center gap-2 transition-transform duration-200 hover:scale-110"
+                        >
+                            <img :src="techno.image" :alt="techno.name" class="h-16 w-16"/>
+                            <span class="text-sm font-medium">{{ techno.name }}</span>
+                        </NuxtLink>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        <p>{{techno.tooltip}}</p>
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
+            
         </div>
     </section>
 </template>
 
 <script lang="ts" setup>
+    import {
+        Tooltip,
+        TooltipContent,
+        TooltipProvider,
+        TooltipTrigger,
+    } from '@/components/ui/tooltip'
+
     const technoItems = [
-        { name: "Laravel", image: "/images/laravel.svg", link: "https://laravel.com/"},
-        { name: "Nuxt", image: "/images/nuxtjs.svg", link: "https://nuxt.com/"},
-        { name: "Tauri", image: "/images/tauri.svg", link: "https://tauri.app/"},
-        { name: "Rust", image: "/images/rust.svg", link: "https://www.rust-lang.org/"},
-        { name: "TypeScript", image: "/images/typescript.svg", link: "https://www.typescriptlang.org/"},
-        { name: "Tailwind", image: "/images/tailwindcss.svg", link: "https://tailwindcss.com/"},
+        { name: "Laravel", image: "/images/laravel.svg", link: "https://laravel.com/" , tooltip: "Pour les APIs RESTful"},
+        { name: "Nuxt", image: "/images/nuxtjs.svg", link: "https://nuxt.com/", tooltip: "Pour les interfaces web avec un bon SEO et les UIs Mobiles et Desktop via Tauri"},
+        { name: "Tauri", image: "/images/tauri.svg", link: "https://tauri.app/", tooltip: "Pour les applications Mobiles et Desktop légères, sécurisées et cross-plateformes"},
+        { name: "Rust", image: "/images/rust.svg", link: "https://www.rust-lang.org/", tooltip: "Pour les applications performantes et sécurisées (logique backend des apps Tauri)"},
+        { name: "TypeScript", image: "/images/typescript.svg", link: "https://www.typescriptlang.org/", tooltip: "Pour un code JavaScript typé, plus sûr et maintenable"},
+        { name: "Tailwind", image: "/images/tailwindcss.svg", link: "https://tailwindcss.com/", tooltip: "Pour un design moderne, réactif et personnalisable"},
     ]
 </script>
 
